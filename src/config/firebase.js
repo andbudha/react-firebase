f1850cd0aa079ce7514637c30c59e65e5ecfb954
andbudha <andreibartov@gmail.com>
2024-05-01 10:58:48 +0200

    config dir added to src dir and gitignore

---
+++ src/config/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAuoAJZUJpcOAs852idvmV7Km3HOrFD1Hc',
+  authDomain: 'fir-auth-crud-dde5f.firebaseapp.com',
+  projectId: 'fir-auth-crud-dde5f',
+  storageBucket: 'fir-auth-crud-dde5f.appspot.com',
+  messagingSenderId: '448252701391',
+  appId: '1:448252701391:web:b73a3b2b273e0daeaaa5ba',
+  measurementId: 'G-048GW77Q77',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
