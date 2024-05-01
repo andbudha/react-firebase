19e790b36f3444022467f82ee944cbf7d619b8a5
andbudha <andreibartov@gmail.com>
2024-05-01 13:46:33 +0200

    signIn func-ty added

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,13 +1,51 @@
 import styles from './Auth.module.css';
+import { auth } from '../../config/firebase.ts';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { ChangeEvent, useState } from 'react';
 
 export const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassowrd] = useState('');
+
+  console.log(auth.currentUser?.email);
+
+  const emailSettingFunc = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const passwordSettingFunc = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassowrd(event.currentTarget.value);
+  };
+  const signIn = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setEmail('');
+      setPassowrd('');
+    }
+  };
   return (
     <div className={styles.main_auth_box}>
       <div className={styles.auth_box}>
-        <input placeholder="Email..." className={styles.login_input} />
-        <input placeholder="Password..." className={styles.login_input} />
+        <input
+          value={email}
+          placeholder="Email..."
+          className={styles.login_input}
+          onChange={emailSettingFunc}
+        />
+        <input
+          type="password"
+          value={password}
+          placeholder="Password..."
+          className={styles.login_input}
+          onChange={passwordSettingFunc}
+        />
         <div className={styles.btn_box}>
-          <button className={styles.login_btn}>Login</button>
+          <button className={styles.login_btn} onClick={signIn}>
+            Login
+          </button>
         </div>
       </div>
     </div>
