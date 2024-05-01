935afdef52ab1ddad0d7e038366688b06f982c61
andbudha <andreibartov@gmail.com>
2024-05-01 22:40:11 +0200

    home and auth routes set

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import styles from './App.module.css';
 import { Auth } from './components/Auth/Auth';
 import { Home } from './components/Home/Home';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.main}>
-      {/* <Auth /> */}
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Auth />} />
+      </Routes>
     </div>
   );
 }
