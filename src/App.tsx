aef93ea620895706cdc1887a06f8227c60993820
andbudha <andreibartov@gmail.com>
2024-05-01 14:15:44 +0200

    Home comp created and styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import styles from './App.module.css';
 import { Auth } from './components/Auth/Auth';
+import { Home } from './components/Home/Home';
 
 function App() {
   return (
     <div className={styles.main}>
-      <Auth />
+      {/* <Auth /> */}
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,8 @@
+import styles from './Home.module.css';
+export const Home = () => {
+  return (
+    <div className={styles.main_home_page}>
+      <h1>Home page</h1>
+    </div>
+  );
+};
