b18c23d5a379195ec073add53223367ff1904245
andbudha <andreibartov@gmail.com>
2024-05-09 15:09:50 +0200

    Toggling movie-form on btn click func-ty added

--- src/components/MovieForm/MovieForm.tsx
+++ src/components/MovieForm/MovieForm.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, useState } from 'react';
 import styles from './Movieform.module.css';
 import { addDoc, collection } from 'firebase/firestore';
 import { dataBase } from '../../config/firebase';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa6';
 
 type MovieFormProps = {
   getData: () => void;
@@ -10,8 +11,12 @@ export const MovieForm = ({ getData }: MovieFormProps) => {
   const [inputTitle, setInputTitle] = useState<string>('');
   const [inputReleaseYear, setInputReleaseYear] = useState<string>('');
   const [answer, setAnswer] = useState<boolean>(false);
+  const [toggleMovieBtn, setToggleMovieBtn] = useState<boolean>(false);
   const movieCollection = collection(dataBase, 'movies');
 
+  const toggleMovieBtnHandler = () => {
+    setToggleMovieBtn(!toggleMovieBtn);
+  };
   const newMovie = {
     oscar: answer,
     releaseYear: Number(inputReleaseYear),
@@ -32,6 +37,7 @@ export const MovieForm = ({ getData }: MovieFormProps) => {
       try {
         const response = await addDoc(movieCollection, newMovie);
         if (response) {
+          setToggleMovieBtn(!toggleMovieBtn);
           getData();
           alert('Movie added successfully');
           setInputTitle('');
@@ -48,51 +54,66 @@ export const MovieForm = ({ getData }: MovieFormProps) => {
   };
   return (
     <div className={styles.movie_form_main_box}>
-      <input
-        value={inputTitle}
-        className={styles.movie_title_input}
-        placeholder="Title..."
-        onChange={getTitleInputValueHandler}
-      />
-      <input
-        value={inputReleaseYear}
-        className={styles.movie_release_year_input}
-        placeholder="Release year..."
-        onChange={getReleaseYearInputValueHandler}
-      />
-      <div className={styles.radio_btn_box}>
-        <h4>Received Oscar: </h4>
-        <div className={styles.answer_box}>
-          <div className={styles.answer}>
-            {' '}
-            <input
-              className={styles.yes_answer_radio_btn}
-              type="radio"
-              name="oscar"
-              id="yes"
-              value="yes"
-              checked={answer === true}
-              onChange={() => setAnswer(true)}
-            />
-            <label htmlFor="yes">Yes</label>
-          </div>
-          <div className={styles.answer}>
-            <input
-              className={styles.no_answer_radio_btn}
-              type="radio"
-              name="oscar"
-              id="no"
-              value="no"
-              checked={answer === false}
-              onChange={() => setAnswer(false)}
-            />
-            <label htmlFor="no">No</label>
-          </div>
-        </div>
+      <div
+        className={styles.toggle_movie_form_btn}
+        onClick={toggleMovieBtnHandler}
+      >
+        <h4>Add Movie</h4>
+        {toggleMovieBtn ? (
+          <FaChevronUp className={styles.chevron_up} />
+        ) : (
+          <FaChevronDown className={styles.chevron_down} />
+        )}
       </div>
-      <button className={styles.add_movie_btn} onClick={addNewMovieHandler}>
-        Add Movie
-      </button>
+      {!!toggleMovieBtn && (
+        <div className={styles.movie_form_box}>
+          <input
+            value={inputTitle}
+            className={styles.movie_title_input}
+            placeholder="Title..."
+            onChange={getTitleInputValueHandler}
+          />
+          <input
+            value={inputReleaseYear}
+            className={styles.movie_release_year_input}
+            placeholder="Release year..."
+            onChange={getReleaseYearInputValueHandler}
+          />
+          <div className={styles.radio_btn_box}>
+            <h4>Received Oscar: </h4>
+            <div className={styles.answer_box}>
+              <div className={styles.answer}>
+                {' '}
+                <input
+                  className={styles.yes_answer_radio_btn}
+                  type="radio"
+                  name="oscar"
+                  id="yes"
+                  value="yes"
+                  checked={answer === true}
+                  onChange={() => setAnswer(true)}
+                />
+                <label htmlFor="yes">Yes</label>
+              </div>
+              <div className={styles.answer}>
+                <input
+                  className={styles.no_answer_radio_btn}
+                  type="radio"
+                  name="oscar"
+                  id="no"
+                  value="no"
+                  checked={answer === false}
+                  onChange={() => setAnswer(false)}
+                />
+                <label htmlFor="no">No</label>
+              </div>
+            </div>
+          </div>
+          <button className={styles.add_movie_btn} onClick={addNewMovieHandler}>
+            Add Movie
+          </button>
+        </div>
+      )}
     </div>
   );
 };
