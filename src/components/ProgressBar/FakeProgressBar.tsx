f3dd97df0d59649887b8f9b5d4faeccbe34b9d5b
andbudha <andreibartov@gmail.com>
2024-05-09 22:02:20 +0200

    ProgressBar&FakeProgressBar created&styled

---
+++ src/components/ProgressBar/FakeProgressBar.tsx
@@ -0,0 +1,5 @@
+import styles from './FakeProgressBar.module.css';
+
+export default function FakeProgressBar() {
+  return <div className={styles.fake_loader_box}></div>;
+}
---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,5 @@
+import styles from './ProgressBar.module.css';
+
+export const ProgressBar = () => {
+  return <div className={styles.loader}></div>;
+};
