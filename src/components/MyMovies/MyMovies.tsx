a5aa8b320b496dd66693301253ac226eea4c068e
andbudha <andreibartov@gmail.com>
2024-05-10 16:25:15 +0200

    MyMovies comp added and sytled

---
+++ src/components/MyMovies/MyMovies.tsx
@@ -0,0 +1,9 @@
+import styles from './MyMovies.module.css';
+
+export const MyMovies = () => {
+  return (
+    <div className={styles.my_movies_main_box}>
+      <h1>MyMovies</h1>
+    </div>
+  );
+};
